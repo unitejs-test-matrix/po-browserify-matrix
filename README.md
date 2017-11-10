aa575c0a2bbb4ed499ad12b164c35c075f9a9330
Martyn Janes <martyn@obany.com>
2017-11-10 09:20:43 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# UniteJS Test Matrix - Polymer Browserify
+
+Repo to run a multitude of configuration options for UniteJS on Polymer Application Framework.
+
+[![Build Status][travis-image]][travis-url]
+
+[travis-url]: https://travis-ci.org/unitejs-test-matrix/po-browserify-matrix/
+[travis-image]: http://img.shields.io/travis/unitejs-test-matrix/po-browserify-matrix/master.svg?style=flat
---
+++ create-apps.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -e
+
+#
+# Polymer Browserify
+#
+node unitejs/cli/bin/unite configure --packageName=po-bfy-js-jas-pro --title="Polymer Browserify JavaScript" --sourceLanguage=JavaScript --moduleType=CommonJS --bundler=Browserify --unitTestRunner=Karma --unitTestFramework=Jasmine --unitTestEngine=ChromeHeadless --e2eTestRunner=Protractor --e2eTestFramework=Jasmine --linter=ESLint --license=MIT --packageManager=Npm --cssPre=Css --cssPost=PostCss --appFramework=Polymer --ides=vscode --disableVersionCheck --outputDirectory=./apps/po-bfy-js-jas-pro
+node unitejs/cli/bin/unite generate --type=class --name=MyClass --disableVersionCheck --outputDirectory=./apps/po-bfy-js-jas-pro
+node unitejs/cli/bin/unite generate --type=component --name=MyComponent --disableVersionCheck --outputDirectory=./apps/po-bfy-js-jas-pro
+node unitejs/cli/bin/unite generate --type=enum --name=MyEnum --disableVersionCheck --outputDirectory=./apps/po-bfy-js-jas-pro
+
+node unitejs/cli/bin/unite configure --packageName=po-bfy-js-jas-wdr --title="Polymer Browserify JavaScript" --sourceLanguage=JavaScript --moduleType=CommonJS --bundler=Browserify --unitTestRunner=Karma --unitTestFramework=Jasmine --unitTestEngine=ChromeHeadless --e2eTestRunner=WebdriverIO --e2eTestFramework=Jasmine --linter=ESLint --license=MIT --packageManager=Npm --cssPre=Sass --cssPost=PostCss --appFramework=Polymer --ides=vscode --disableVersionCheck --outputDirectory=./apps/po-bfy-js-jas-wdr
+node unitejs/cli/bin/unite generate --type=class --name=MyClass --disableVersionCheck --outputDirectory=./apps/po-bfy-js-jas-wdr
+node unitejs/cli/bin/unite generate --type=component --name=MyComponent --disableVersionCheck --outputDirectory=./apps/po-bfy-js-jas-wdr
+node unitejs/cli/bin/unite generate --type=enum --name=MyEnum --disableVersionCheck --outputDirectory=./apps/po-bfy-js-jas-wdr
+
+node unitejs/cli/bin/unite configure --packageName=po-bfy-js-mch-pro --title="Polymer Browserify JavaScript" --sourceLanguage=JavaScript --moduleType=CommonJS --bundler=Browserify --unitTestRunner=Karma --unitTestFramework=MochaChai --unitTestEngine=ChromeHeadless --e2eTestRunner=Protractor --e2eTestFramework=MochaChai --linter=ESLint --license=MIT --packageManager=Npm --cssPre=Less --cssPost=PostCss --appFramework=Polymer --ides=vscode --disableVersionCheck --outputDirectory=./apps/po-bfy-js-mch-pro
+node unitejs/cli/bin/unite generate --type=class --name=MyClass --disableVersionCheck --outputDirectory=./apps/po-bfy-js-mch-pro
+node unitejs/cli/bin/unite generate --type=component --name=MyComponent --disableVersionCheck --outputDirectory=./apps/po-bfy-js-mch-pro
+node unitejs/cli/bin/unite generate --type=enum --name=MyEnum --disableVersionCheck --outputDirectory=./apps/po-bfy-js-mch-pro
+
+node unitejs/cli/bin/unite configure --packageName=po-bfy-js-mch-wdr --title="Polymer Browserify JavaScript" --sourceLanguage=JavaScript --moduleType=CommonJS --bundler=Browserify --unitTestRunner=Karma --unitTestFramework=MochaChai --unitTestEngine=ChromeHeadless --e2eTestRunner=WebdriverIO --e2eTestFramework=MochaChai --linter=ESLint --license=MIT --packageManager=Npm --cssPre=Stylus --cssPost=PostCss --appFramework=Polymer --ides=vscode --disableVersionCheck --outputDirectory=./apps/po-bfy-js-mch-wdr
+node unitejs/cli/bin/unite generate --type=class --name=MyClass --disableVersionCheck --outputDirectory=./apps/po-bfy-js-mch-wdr
+node unitejs/cli/bin/unite generate --type=component --name=MyComponent --disableVersionCheck --outputDirectory=./apps/po-bfy-js-mch-wdr
+node unitejs/cli/bin/unite generate --type=enum --name=MyEnum --disableVersionCheck --outputDirectory=./apps/po-bfy-js-mch-wdr
+
+node unitejs/cli/bin/unite configure --packageName=po-bfy-ts-jas-pro --title="Polymer Browserify TypeScript" --sourceLanguage=TypeScript --moduleType=CommonJS --bundler=Browserify --unitTestRunner=Karma --unitTestFramework=Jasmine --unitTestEngine=ChromeHeadless --e2eTestRunner=Protractor --e2eTestFramework=Jasmine --linter=TSLint --license=MIT --packageManager=Npm --cssPre=Css --cssPost=None --appFramework=Polymer --ides=vscode --disableVersionCheck --outputDirectory=./apps/po-bfy-ts-jas-pro
+node unitejs/cli/bin/unite generate --type=class --name=MyClass --disableVersionCheck --outputDirectory=./apps/po-bfy-ts-jas-pro
+node unitejs/cli/bin/unite generate --type=component --name=MyComponent --disableVersionCheck --outputDirectory=./apps/po-bfy-ts-jas-pro
+node unitejs/cli/bin/unite generate --type=enum --name=MyEnum --disableVersionCheck --outputDirectory=./apps/po-bfy-ts-jas-pro
+node unitejs/cli/bin/unite generate --type=interface --name=MyInterface --disableVersionCheck --outputDirectory=./apps/po-bfy-ts-jas-pro
+
+node unitejs/cli/bin/unite configure --packageName=po-bfy-ts-jas-wdr --title="Polymer Browserify TypeScript" --sourceLanguage=TypeScript --moduleType=CommonJS --bundler=Browserify --unitTestRunner=Karma --unitTestFramework=Jasmine --unitTestEngine=ChromeHeadless --e2eTestRunner=WebdriverIO --e2eTestFramework=Jasmine --linter=TSLint --license=MIT --packageManager=Npm --cssPre=Sass --cssPost=None --appFramework=Polymer --ides=vscode --disableVersionCheck --outputDirectory=./apps/po-bfy-ts-jas-wdr
+node unitejs/cli/bin/unite generate --type=class --name=MyClass --disableVersionCheck --outputDirectory=./apps/po-bfy-ts-jas-wdr
+node unitejs/cli/bin/unite generate --type=component --name=MyComponent --disableVersionCheck --outputDirectory=./apps/po-bfy-ts-jas-wdr
+node unitejs/cli/bin/unite generate --type=enum --name=MyEnum --disableVersionCheck --outputDirectory=./apps/po-bfy-ts-jas-wdr
+node unitejs/cli/bin/unite generate --type=interface --name=MyInterface --disableVersionCheck --outputDirectory=./apps/po-bfy-ts-jas-wdr
+
+node unitejs/cli/bin/unite configure --packageName=po-bfy-ts-mch-pro --title="Polymer Browserify TypeScript" --sourceLanguage=TypeScript --moduleType=CommonJS --bundler=Browserify --unitTestRunner=Karma --unitTestFramework=MochaChai --unitTestEngine=ChromeHeadless --e2eTestRunner=Protractor --e2eTestFramework=MochaChai --linter=TSLint --license=MIT --packageManager=Npm --cssPre=Less --cssPost=None --appFramework=Polymer --ides=vscode --disableVersionCheck --outputDirectory=./apps/po-bfy-ts-mch-pro
+node unitejs/cli/bin/unite generate --type=class --name=MyClass --disableVersionCheck --outputDirectory=./apps/po-bfy-ts-mch-pro
+node unitejs/cli/bin/unite generate --type=component --name=MyComponent --disableVersionCheck --outputDirectory=./apps/po-bfy-ts-mch-pro
+node unitejs/cli/bin/unite generate --type=enum --name=MyEnum --disableVersionCheck --outputDirectory=./apps/po-bfy-ts-mch-pro
+node unitejs/cli/bin/unite generate --type=interface --name=MyInterface --disableVersionCheck --outputDirectory=./apps/po-bfy-ts-mch-pro
+
+node unitejs/cli/bin/unite configure --packageName=po-bfy-ts-mch-wdr --title="Polymer Browserify TypeScript" --sourceLanguage=TypeScript --moduleType=CommonJS --bundler=Browserify --unitTestRunner=Karma --unitTestFramework=MochaChai --unitTestEngine=ChromeHeadless --e2eTestRunner=WebdriverIO --e2eTestFramework=MochaChai --linter=TSLint --license=MIT --packageManager=Npm --cssPre=Stylus --cssPost=None --appFramework=Polymer --ides=vscode --disableVersionCheck --outputDirectory=./apps/po-bfy-ts-mch-wdr
+node unitejs/cli/bin/unite generate --type=class --name=MyClass --disableVersionCheck --outputDirectory=./apps/po-bfy-ts-mch-wdr
+node unitejs/cli/bin/unite generate --type=component --name=MyComponent --disableVersionCheck --outputDirectory=./apps/po-bfy-ts-mch-wdr
+node unitejs/cli/bin/unite generate --type=enum --name=MyEnum --disableVersionCheck --outputDirectory=./apps/po-bfy-ts-mch-wdr
+node unitejs/cli/bin/unite generate --type=interface --name=MyInterface --disableVersionCheck --outputDirectory=./apps/po-bfy-ts-mch-wdr
